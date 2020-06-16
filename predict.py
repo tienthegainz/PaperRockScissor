ae4c1984ad08e573ba077f5bcc2a6ad3e59b0e6d
Tien <viettienha98@gmail.com>
2020-06-16 09:40:35 +0700

    First commit

---
+++ predict.py
@@ -0,0 +1,47 @@
+from mobilenet_v2 import mobilenet_v2, MobileNetV2
+from PIL import Image
+from torchvision import transforms
+import torch
+import torch.nn.functional as F
+
+
+def preprocess_image(pil_image):
+    image = transforms.ToTensor()(pil_image).unsqueeze_(0).cpu()
+    image /= 255.00
+    image = F.interpolate(image, size=256)
+    return image
+
+
+if __name__ == "__main__":
+    ########################### Magic code line #################################
+    checkpoint = torch.load('MBN_epoch_1_loss_0.07.pth',
+                            map_location=torch.device('cpu'))
+    # print(checkpoint)
+    model = MobileNetV2(num_classes=3)
+    model.load_state_dict(checkpoint)
+    # print(model)
+    model.eval()
+    # Your image here
+    pil_image1 = Image.open('test_data/dam.jpg')
+    image1 = preprocess_image(pil_image1)
+    # print(image.shape)
+    output = model(image1)
+    print(output)
+    _, predicted = torch.max(output.data, 1)
+    print(predicted)
+
+    pil_image2 = Image.open('test_data/keo.jpg')
+    image2 = preprocess_image(pil_image2)
+    # print(image.shape)
+    output = model(image2)
+    print(output)
+    _, predicted = torch.max(output.data, 1)
+    print(predicted)
+
+    pil_image3 = Image.open('test_data/la.jpg')
+    image3 = preprocess_image(pil_image3)
+    # print(image.shape)
+    output = model(image3)
+    print(output)
+    _, predicted = torch.max(output.data, 1)
+    print(predicted)
